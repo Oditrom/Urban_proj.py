2b03f8de8fabe12f35a982295a1c536f31f0cdc8
John Doe <sergedoc@yandex.ru>
2024-12-02 18:42:06 +0300

    1

--- recurs.py
+++ recurs.py
@@ -1,8 +1,12 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-    first = int(str(number[0])) 
-    if len(str(number)) > 1 and str(number[0]) !=0:
+    first = int(str_number[0]) 
+    if len(str(number)) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
+    if number == 0:
+        return 1
     else:
-        return first   
-print(get_multiplied_digits(402030))  
+        return first    
+    
+
+
