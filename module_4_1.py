f879f7316c1e96495b74cd7f95b3efa2e492d819
Sergey Kolesnikov <sergedoc@yandex.ru>
2024-12-04 23:25:22 +0300

    1

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as fake_divine
+from true_math import divide as true_divide
+
+
+
+#result_1 = fake_divine(65,0)
+#result_2 = true_divide(15,0)
+
+
+#print(result_1)
+#print(result_2)
